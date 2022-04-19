8acdc1e4084420ecf5a4d1c7a9e827b2abf25458
codingbe <voluntad3000@gmail.com>
2022-04-19 23:16:19 +0900

    Time: 320 ms (6.88%), Space: 51.4 MB (9.75%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.js
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -4,54 +4,23 @@
  */
 const romanToInt = (s) => {
     let result = 0;
+    const symbol = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
     for(let i=0; i<s.length; i++) {
-        if(s[i]+s[i+1] === "IV" || s[i]+s[i+1] === "IX" || s[i]+s[i+1] === "XL" || s[i]+s[i+1] === "XC" || s[i]+s[i+1] === "CD" || s[i]+s[i+1] === "CM") {
-            result += exce(s[i+1]);
-            i += 1;
+        if(s[i]+s[i+1] === "IV" || s[i]+s[i+1] === "IX" || s[i]+s[i+1] === "XL" || s[i]+s[i+1] === "XC" || s[i]+s[i+1] === "CD" || s[i]+s[i+1] === "CM"){
+            result += symbol[s[i+1]] - symbol[s[i]];
+            i+=1;
         }
         else {
-            result += calValue(s[i]);
+            result += symbol[s[i]];
         }
     }
     return result;
 };
-
-const calValue = (s) => {
-    switch(s) {
-        case "M":
-            return 1000;
-        case "D":
-            return 500;
-        case "C":
-            return 100;
-        case "L":
-            return 50;
-        case "X":
-            return 10;
-        case "V":
-            return 5;
-        case "I":
-            return 1;
-        default:
-            break;
-    }
-}
-
-const exce = (s) => {
-    switch(s) {
-        case "V":
-            return 4;
-        case "X":
-            return 9;
-        case "L":
-            return 40;
-        case "C":
-            return 90;
-        case "D":
-            return 400;
-        case "M":
-            return 900;
-        default:
-            break;
-    }
-}
