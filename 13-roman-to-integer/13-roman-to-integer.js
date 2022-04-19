fd35a7dfcad33e9db32ba2d3df64eaa97537992f
codingbe <voluntad3000@gmail.com>
2022-04-19 23:39:51 +0900

    Time: 149 ms (74.13%), Space: 48.5 MB (26.07%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.js
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -16,7 +16,8 @@ const romanToInt = (s) => {
     }
     for(let i=0; i<s.length; i++) {
         if(exec.indexOf(s[i]+s[i+1]) !== -1) {
-            result -= symbol[s[i]];
+            result += symbol[s[i+1]] - symbol[s[i]];
+            i+=1;
         }
         else {
             result += symbol[s[i]];
