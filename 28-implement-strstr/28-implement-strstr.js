486fb41ba20e3a23c9b746d4cd72a6608ef742b4
codingbe <voluntad3000@gmail.com>
2022-04-21 23:55:58 +0900

    Time: 70 ms (76.07%), Space: 42.3 MB (59.43%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.js
+++ 28-implement-strstr/28-implement-strstr.js
@@ -3,6 +3,8 @@
  * @param {string} needle
  * @return {number}
  */
-const strStr = function(haystack, needle) {
-    return haystack.indexOf(needle);
+const strStr = function (haystack, needle) {
+   const temp = haystack.split(needle);
+   if (temp.length === 1) return -1;
+   else return temp[0].length;
 };
