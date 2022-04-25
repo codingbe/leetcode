958208768978197c46710752951f7d8bb6010963
codingbe <voluntad3000@gmail.com>
2022-04-25 15:56:58 +0900

    Time: 76 ms (55.07%), Space: 42.1 MB (55.39%) - LeetHub

--- 27-remove-element/27-remove-element.js
+++ 27-remove-element/27-remove-element.js
@@ -5,10 +5,12 @@
  */
 
 const removeElement = function (nums, val) {
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === val) {
-      nums.splice(i, 1);
-      i--;
+  let i = 0;
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== val) {
+      nums[i] = nums[j];
+      i++;
     }
   }
+  return i;
 };
