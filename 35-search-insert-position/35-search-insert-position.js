97b08a104d0206e11ac93c314cd1fbd69057fafb
codingbe <voluntad3000@gmail.com>
2022-04-25 16:34:58 +0900

    Time: 64 ms (81.59%), Space: 42.2 MB (67.72%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -4,19 +4,15 @@
  * @return {number}
  */
 const searchInsert = function (nums, target) {
-   let start = 0;
-   let end = nums.length - 1;
-   let mid = Math.floor((end + start) / 2);
+  let start = 0;
+  let end = nums.length - 1;
+  let mid = Math.floor((end + start) / 2);
 
-   while (end - start >= 0) {
-      if (nums[mid] === target) {
-         return mid;
-      } else if (nums[mid] < target) {
-         start = mid + 1;
-      } else if (nums[mid] > target) {
-         end = mid - 1;
-      }
-      mid = Math.floor((end + start) / 2);
-   }
-   return mid+1;
+  while (end - start >= 0) {
+    if (nums[mid] === target) return mid;
+    else if (nums[mid] < target) start = mid + 1;
+    else if (nums[mid] > target) end = mid - 1;
+    mid = Math.floor((end + start) / 2);
+  }
+  return mid + 1;
 };
