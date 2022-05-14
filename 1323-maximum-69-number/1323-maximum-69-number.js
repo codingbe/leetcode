5af91fd54afc2beb1dce0b79e2106305681e4e14
codingbe <voluntad3000@gmail.com>
2022-05-14 23:21:28 +0900

    Time: 77 ms (54.31%), Space: 42.5 MB (6.37%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+const maximum69Number = function (num) {
+  const answer = [num];
+  const arr = String(num)
+    .split("")
+    .map((num) => parseInt(num));
+
+  for (let i = 0; i < arr.length; i++) {
+    const temp = arr.slice(0);
+    temp[i] === 6 ? (temp[i] = 9) : (temp[i] = 6);
+    answer.push(parseInt(temp.join("")));
+  }
+  return Math.max(...answer);
+};
